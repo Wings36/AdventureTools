e43c5090126fcd3f6f665b3ab6e265310c85a91b
BuildTools <unconfigured@null.spigotmc.org>
2021-03-15 04:13:38 -0700

    Implemented looking up player coordinates and setting player compass coordinates.

--- src/main/java/me/nexusnetwork/adventuretools/compass/compassTuner.java
+++ src/main/java/me/nexusnetwork/adventuretools/compass/compassTuner.java
@@ -1,6 +1,42 @@
 package me.nexusnetwork.adventuretools.compass;
 
-public class compassTuner {
-//test
-    //test 2
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class compassTuner implements CommandExecutor {
+
+    Plugin plugin;
+
+    // /compass {set/lookup/cancel}
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command compass, @NotNull String label, @NotNull String[] args) {
+        if (sender.isOp()) {
+            if (args.length == 5) {
+                //Set command
+                if (args[0].toLowerCase().equals("set")) {
+                    Player target = plugin.getServer().getPlayer(args[1]);
+                    Location destination = new Location(target.getWorld(), Double.parseDouble(args[2]), Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+                    target.setCompassTarget(destination);
+                }
+                //Look up
+                if (args[0].toLowerCase().equals("lookup")) {
+                    Player target = plugin.getServer().getPlayer(args[1]);
+                    Location point = target.getLocation();
+                    sender.sendMessage(target.getName() + " is currently on world " + point.getWorld().getName() +
+                            "\nat coords X: " + point.getX() + " Y: " + point.getY() + " Z: " + point.getZ());
+                }
+            }
+            else
+                return false;
+        }
+        else {
+            sender.sendMessage("You do not have permission!");
+        }
+        return true;
+    }
 }
